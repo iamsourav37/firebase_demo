f8c1dcfe891c274877a986984080b6335a7f9bcf
iamSourav37 <souravsrk120@gmail.com>
2020-10-02 10:26:07 +0530

    removed unused import statement

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'dart:developer';
 
-import 'package:flutter/services.dart';
 
 class LoginPage extends StatefulWidget {
   @override
